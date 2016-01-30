f71f6e6d1ec2f0364b34e84952c2ec617820c1a1
Justin Love <git@JustinLove.name>
2016-01-30 14:39:33 -0600

    untested backwards compatiblity interface

--- ui/mods/icon_reloader/atlas_message.js
+++ ui/mods/icon_reloader/atlas_message.js
@@ -1,149 +1,36 @@
 ;(function() {
-  var lookupPage = function(pageName) {
-    var def = $.Deferred()
-    var findPage = function(pages) {
-      //console.log(pages)
-      var page = pages.filter(function(page) {
-        return page.url.match('/'+pageName+'.html')
-      })[0]
-      def.resolve(page)
-    }
-
-    $.get('http://127.0.0.1:9999/json').then(findPage)
-
-    return def.promise()
-  }
-
-  var load = function(pageName, script) {
-    evaluateOnPage(pageName, loadScript(script))
-  }
-
-  var loadScript = function(script) {
-    return 'loadScript("' + script + '")'
-  }
-
-  var message = function(pageName, message, payload) {
-    evaluateOnPage(pageName, invokeHandler(message, payload))
-  }
-
-  var invokeHandler = function(message, payload) {
-    var args = ''
-    if (payload) {
-      args = JSON.stringify(payload)
-    }
-    return 'handlers["' + message + '"](' + args + ')'
-  }
-
-  var evaluateOnPage = function(pageName, expression) {
-    debugMessage(pageName, 'Runtime.evaluate', {expression: expression})
-  }
-
-  var mailboxes = {}
-  var Mailbox = function(pageName) {
-    if (mailboxes[pageName]) return mailboxes[pageName]
-
-    this.pageName = pageName
-    this.messages = []
-    this.pending = 0
-    this.lookupPending = false
-    mailboxes[pageName] = this
-    return this
-  }
-
-  Mailbox.prototype.flush = function() {
-    //console.log('flush')
-    while (this.messages.length > 0) {
-      this.pending++
-      var message = this.messages.shift()
-      //console.log('sending', message)
-      this.ws.send(message)
-    }
-  }
-  Mailbox.prototype.send = function(method, params) {
-    //console.log('mb send', method,params)
-    this.messages.push(JSON.stringify({
-      id: 0,
-      method: method,
-      params: params
-    }))
-    if (this.ws) {
-      if (this.ws.readyState == 1) {
-        this.flush()
-      }
-    } else if (!this.lookupPending) {
-      this.connect()
-    }
-  }
-  Mailbox.prototype.receive = function(message) {
-    if (message && message.data) {
-      var data = JSON.parse(message.data)
-      if (data.wasThrown) {
-        //console.log('data:error', data.result.description)
-      } else {
-        //console.log('data', data.result.result)
-      }
-    } else {
-      //console.log('message', message)
-    }
-    this.pending--
-    if (this.pending < 1 && this.messages.length < 1) {
-      this.close()
-    }
-  }
-  Mailbox.prototype.connect = function() {
-    var mb = this
-    //console.log('connect', mb.ws, mb.lookupPending)
-    if (mb.ws || mb.lookupPending) return
-
-    mb.lookupPending = true
-    lookupPage(this.pageName).then(function(page) {
-      mb.lookupPending = false
-      mb.openSocket(page)
+  var message = function(mailbox, message, payload) {
+    var boxName = 'mailbox:'+mailbox
+    api.memory.load(boxName).then(function(value) {
+      var mb = decode(value) || []
+      mb.push({message: message, payload: payload})
+      api.memory.store(boxName, encode(mb))
     })
   }
-  Mailbox.prototype.openSocket = function(page) {
-    if (!page) return
-
-    var mb = this
-    ws = mb.ws = new WebSocket('ws://127.0.0.1:9999/devtools/page/' + page.id);
-    ws.onmessage = function(message) {
-      //console.log('message')
-      mb.receive(message)
-    }
-    ws.onerror = function(error) {
-      console.error('error', error);
-      mb.close()
-    }
-    ws.onopen = function() {
-      //console.log('open')
-      mb.flush()
-    }
-    ws.onclose = function() {
-      //console.log('close')
-      mb.ws = null
-    }
-  }
-  Mailbox.prototype.close = function() {
-    this.ws.close()
-    this.ws = null
-  }
-
-  var debugMessage = function(pageName, method, params) {
-    if (!pageName) return
-
-    var mb = new Mailbox(pageName)
-    mb.send(method, params)
-  }
-  var close = function() {
-    mailboxes.forEach(function(mb) {
-      if (mb.ws) {
-        mb.close()
-      }
+  var check = function(mailbox) {
+    var boxName = 'mailbox:'+mailbox
+    api.memory.load(boxName).then(function(value) {
+      var mb = decode(value) || []
+      mb.forEach(function(item) {
+        if (handlers[item.message]) {
+          handlers[item.message](item.payload)
+        }
+      })
+      api.memory.store(boxName, encode([]))
     })
+  }
+  var poll = function(mailbox, timeout) {
+    check(mailbox)
+    setTimeout(poll, timeout, mailbox, timeout)
   }
 
   window.atlasMessage = window.atlasMessage || {}
-  atlasMessage.load = load
   atlasMessage.message = message
-  atlasMessage.close = close
+  atlasMessage.check = check
+  atlasMessage.poll = poll
+
+  // prior version stubs
+  atlasMessage.load = function() {}
+  atlasMessage.close = function() {}
 })()
+
--- ui/mods/icon_reloader/icon_atlas.js
+++ ui/mods/icon_reloader/icon_atlas.js
@@ -1,119 +1,15 @@
 (function() {
   console.log('loaded icon atlas')
 
-  var testNoFreeSlots = false
-
-  if (!model.stockStrategicIcons) {
-    console.log('capture stock strategic icons')
-    model.stockStrategicIcons = model.strategicIcons().slice(0)
-  }
-
   model.iconPool = model.iconPool || []
 
-  // two entries, second is used by game
-  var duplicateCommander = model.stockStrategicIcons.filter(function(icon) {
-    return icon == 'commander'
-  }).length > 1
-  if (duplicateCommander) {
-    model.iconPool.push('commander')
-  }
-  model.iconPool = model.iconPool.concat([
-    'energy_storage_adv',
-    'metal_storage_adv',
-    'tank_lava',
-    'paratrooper',
-    'tutorial_titan_commander',
-    'metal_spot_preview',
-    'avatar',
-    //'deep_space_radar', // see our live_game.js
-  ])
-
-  model.resetCustomIcons = function() {
-    model.strategicIcons(model.stockStrategicIcons.slice(0))
-    model.slotUsage = model.strategicIcons().map(function(id) {
-      return [id]
-    })
-    if (!testNoFreeSlots) {
-      model.iconPool.forEach(function(id) {
-        var i = model.strategicIcons().indexOf(id)
-        //console.log('unused?', id, i)
-        if (i > -1) {
-          model.slotUsage[i] = []
-        }
-      })
-    }
-  }
-
-  var assignedIndex = function(id) {
-    for (var index = 0;index < model.slotUsage.length;index++) {
-      var i = model.slotUsage[index].indexOf(id)
-      if (i > -1) return index
-    }
-
-    return -1
-  }
-
-  var emptySlot = function() {
-    for (var i = 0;i < model.slotUsage.length;i++) {
-      if (model.slotUsage[i].length < 1) return i
-    }
-
-    return -1
-  }
-
-  var assign = function(to, i) {
-    var from = model.strategicIcons()[i]
-    console.log('replacing', from, to)
-    model.strategicIcons()[i] = to
-    model.slotUsage[i].push(to)
-  }
-
-  var alias = function(to, i) {
-    var from = model.strategicIcons()[i]
-    console.log('alias', from, to)
-    model.slotUsage[i].push(to)
-  }
-
-  var release = function(to) {
-    model.slotUsage.forEach(function(slot) {
-      var i = slot.indexOf(to)
-      if (i > -1) {
-        slot.splice(i, 1)
-      }
-    })
-  }
-
   var tryToAssignString = function(to) {
-    var a = assignedIndex(to)
-    if (a > -1) return a
-
-    var e = emptySlot()
-    if (e > -1) {
-      assign(to, e)
-      return e
-    }
-
-    return -1
-  }
-
-  var tryToAssignAlias = function(to, aliases) {
-    for (var i in aliases) {
-      var a = assignedIndex(aliases[i])
-      if (a > -1) {
-        alias(to, a)
-        return a
-      }
-    }
-
-    return -1
+    model.strategicIcons.push(to)
   }
 
   var tryToAssignObject = function(obj) {
     var to = Object.keys(obj)[0]
-    var s = tryToAssignString(to)
-    if (s > -1) return s
-
-    return tryToAssignAlias(to, obj[to])
+    tryToAssignString(to)
   }
 
   handlers.request_icons = function(icons) {
@@ -128,14 +24,10 @@
     model.strategicIcons.notifySubscribers()
   }
 
-  handlers.release_icons = function(icons) {
-    if (!testNoFreeSlots) {
-      _.uniq(icons).forEach(release)
-    }
-  }
+  handlers.release_icons = function(icons) {}
 
   handlers.update_and_freeze_icon_changes = function() {
-    sendIconListLater()
+    model.sendIconList()
   }
 
   handlers.reload_icons = function() {
@@ -143,78 +35,10 @@
     $('img').attr('src', function(i, v) {
       return v.split('?')[0] + '?' + Date.now().toString()
     })
-    deferIconList()
   }
 
-  var sendIconListLater = function() {
-    if (model.pendingIconList) {
-      clearTimeout(model.pendingIconList)
-    }
-    model.pendingIconList = setTimeout(function() {
-      model.pendingIconList = null
+  model.strategicIcons.subscribe(model.sendIconList)
 
-      var unready = 0
-      $('img').each(function() {
-        if (!this.complete) unready++
-      })
-
-      if (unready > 0) {
-        console.log(unready, 'unready')
-        sendIconListLater()
-        return
-      }
-
-      sendLayered()
-    }, 5000)
-  }
-
-  var sendLayered = function() {
-    var list = model.strategicIcons();
-
-    var layers = Math.max.apply(Math, model.slotUsage.map(function(slot) {
-      return slot.length
-    }))
-
-    console.log('sending icons, no more changes will be reflected. layers:', layers)
-    for (var l = layers-1;l >= 0;l--) {
-      model.slotUsage.forEach(function(slot, i) {
-        if (slot[l]) {
-          list[i] = slot[l]
-        }
-      })
-      //console.log(l, list.slice(0))
-      engine.call('handle_icon_list', list, 52);
-    }
-  }
-
-  // reset timer to ensure page has time to load and repaint
-  var deferIconList = function() {
-    if (model.pendingIconList) {
-      sendIconListLater()
-    }
-  }
-
-  model.strategicIcons.subscribe(deferIconList)
-
-  var pollLiveGame = function() {
-    var findPage = function(pages) {
-      //console.log(pages)
-      var page = pages.filter(function(page) {
-        return page.url == "coui://ui/main/game/live_game/live_game.html"
-      })[0]
-      if (!!page) {
-        setTimeout(pollLiveGame, 5000)
-      } else {
-        model.resetCustomIcons()
-        handlers.reload_icons()
-        sendIconListLater()
-      }
-    }
-
-    $.get('http://127.0.0.1:9999/json').then(findPage)
-  }
-
-  model.resetCustomIcons()
   handlers.reload_icons()
-  pollLiveGame()
+  atlasMessage.poll('icon_atlas', 100)
 })()
