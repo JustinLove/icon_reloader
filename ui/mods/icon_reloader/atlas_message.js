f9d496fa166adf4291e77c345ff56bcd3f864141
Justin Love <git@JustinLove.name>
2016-02-18 16:32:04 -0600

    ordered message receive

--- ui/mods/icon_reloader/atlas_message.js
+++ ui/mods/icon_reloader/atlas_message.js
@@ -1,4 +1,13 @@
 ;(function() {
+  var findN = /\d+$/
+  var byId = function(a, b) {
+    var an = parseInt(a.match(findN), 10)
+    var bn = parseInt(b.match(findN), 10)
+    if (an < bn) return -1
+    else if (bn < an) return 1
+    else return 0
+  }
+
   var id = 0
   var ack = []
   var message = function(mailbox, message, payload) {
@@ -9,14 +18,15 @@
   }
   var check = function(mailbox) {
     var prefix = 'mailbox:'+mailbox+':'
+    //console.log('check', prefix)
     api.memory.list(prefix).then(function(boxes) {
       boxes = _.difference(boxes, ack)
       if (boxes.length < 1) return
       ack = ack.concat(boxes)
-      boxes.forEach(function(boxName) {
+      boxes.sort(byId).forEach(function(boxName) {
         api.memory.load(boxName).then(function(value) {
           var item = decode(value) || []
-          //console.log('receive', boxName, item)
+          console.log('receive', boxName, item)
           if (handlers[item.message]) {
             handlers[item.message](item.payload)
           }
